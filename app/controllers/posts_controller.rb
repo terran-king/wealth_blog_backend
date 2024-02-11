5970b2d105a419df77f70c2bc7362e9b962adb37
terran-king <terranlking@gmail.com>
2024-02-11 11:23:38 -0800

    added update and delete with test coverage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    head :no_content
+  end
+
 
   private
 
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe PostsController, type: :controller do
         }.to change(Post, :count).by(1)
       end
     end
+
     context "with invalid title" do
       it "returns a failure response" do
         # expect {
@@ -35,6 +36,7 @@ RSpec.describe PostsController, type: :controller do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+
     context "with invalid body" do
       it "returns a failure response" do
         post :create, params: {post: {title: "This should fail", body: nil}}
@@ -42,4 +44,76 @@ RSpec.describe PostsController, type: :controller do
       end
     end
   end
+
+  describe "DELETE #delete" do
+    context "with valid params" do
+      let!(:blog_post) { create(:post)}
+
+      it "delete an existing Post" do
+        expect{
+          post :destroy, params: {id: blog_post.id}
+        }.to change(Post, :count).by(-1)
+
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context "where doesn't exist" do
+      it "returns a failure response" do
+        # expect {
+        #   post :create, params: {post: {title: nil, body: 'This should fails'}}
+        # }.to have_http_status(:unprocessable_entity)
+        post :create, params: {post: {title: nil, body: 'This should fail'}}
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with valid params" do
+      let!(:blog_post) { create(:post)}
+
+      let(:valid_params) { {id:blog_post.id, post: { title: 'Update Title', body: 'Update Body'}}}
+
+      it "update an existing Post" do
+        put :update, params: valid_params
+        blog_post.reload
+
+        expect(blog_post.title).to match('Update Title')
+        expect(blog_post.body).to match('Update Body')
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with valid title params" do
+      let!(:blog_post) { create(:post)}
+
+      let(:valid_params) { {id:blog_post.id, post: { title: 'Update Title'}}}
+
+      it "update an existing Post" do
+        put :update, params: valid_params
+        blog_post.reload
+
+        expect(blog_post.title).to match('Update Title')
+        expect(blog_post.body).to match('Sample post content.')
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with valid body params" do
+      let!(:blog_post) { create(:post)}
+
+      let(:valid_params) { {id:blog_post.id, post: { body: 'Update Body'}}}
+
+      it "update an existing Post" do
+        put :update, params: valid_params
+        blog_post.reload
+
+        expect(blog_post.title).to match('Sample Post Title')
+        expect(blog_post.body).to match('Update Body')
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
 end
